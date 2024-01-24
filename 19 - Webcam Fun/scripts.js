ebee379fd20957b40fff93035e9025445e7bef6b
wei <wei02919@gmail.com>
2024-01-24 16:42:13 +0800

    prac 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,39 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = ()=>{
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false}).then((stream)=>{
+    video.srcObject = stream
+    video.play()  
+  }).catch((err)=>{
+    console.error(err)
+  })
+}
+
+// getVideo()
+
+const takePhoto = ()=>{
+  snap.play()
+  ctx.drawImage(video, 0, 0, 100, 100);
+  const data = canvas.toDataURL("image/png");
+  const link = document.createElement('a')
+  link.href = data
+  link.setAttribute('download', 'cam-img')
+  link.innerHTML = `<img src = ${data} alt= "cam-img">`
+  strip.insertBefore(link, strip.firstChild);  
+}
+
+video.addEventListener("canplay",()=>{
+  let width = video.videoWidth
+  let height = video.videoHeight
+
+  canvas.width = width
+  canvas.height = height
+
+  let int = setInterval(()=>{
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 26)
+  clearInterval(int)
+});
+
